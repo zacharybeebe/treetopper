b6fade88e0e1dd00f466e0174204558944992d83
Zach Beebe <z.beebe@yahoo.com>
2021-06-21 12:08:12 -0700

    ..

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ keywords = ['forestry', 'natural resources', 'natural', 'resources',
             'timber', 'tree', 'trees', 'west', 'coast', 'west coast', 'logging',
             'cruising', 'scaling', 'inventory', 'forests', 'board feet', 'cubic feet',
             'DBH', 'diameter at breast height', 'DIB', 'diameter inside bark', 'RD',
-            'relative density', 'HDR', 'height to diamater ratio', 'species', 'VBAR',
+            'relative density', 'HDR', 'height to diameter ratio', 'species', 'VBAR',
             'volume to basal area ratio', 'BA', 'basal area', 'TPA', 'trees per acre',
             'scribner', 'timber marketing', 'forest marketing', 'timber management',
             'forest management']
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.txt", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -21,7 +21,7 @@ setuptools.setup(
     description = 'Python module for timber inventory for tree species of the west coast',
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = 'https://github.com/zacharybeebe/timberscale',
+    url = 'https://github.com/zacharybeebe/treetopper',
     license = 'MIT',
     packages = setuptools.find_packages(),
     classifiers = [
@@ -32,5 +32,5 @@ setuptools.setup(
         "Operating System :: OS Independent"],
     keywords = keywords,
     python_requires = '>=3.6',
-    py_modules = ['timberscale']
+    py_modules = ['treetopper']
 )

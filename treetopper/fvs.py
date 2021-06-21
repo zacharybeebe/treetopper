b10345ea4ad5bd4c197abac147ac07af4f5e5bf2
Zach Beebe <z.beebe@yahoo.com>
2021-06-21 12:22:28 -0700

    ..

--- treetopper/fvs.py
+++ treetopper/fvs.py
@@ -5,7 +5,7 @@ from copy import deepcopy
 from datetime import date
 from openpyxl import load_workbook, Workbook
 from openpyxl.styles import Alignment
-from win32com.client import Dispatch
+#from win32com.client import Dispatch
 from pyodbc import connect as pycon
 from sqlite3 import connect as sqcon
 from _constants import (ACCESS_GROUPS_COLS,

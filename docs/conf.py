99d80bdf99da272d1ce0d114b52218754223d960
Zach Beebe <z.beebe@yahoo.com>
2021-06-21 11:20:35 -0700

    ..

--- docs/conf.py
+++ docs/conf.py
@@ -33,8 +33,7 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'myst-parser'
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
